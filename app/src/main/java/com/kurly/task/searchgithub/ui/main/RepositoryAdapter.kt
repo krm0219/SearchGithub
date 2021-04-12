51e48c173f31d394a35bc49cd36756f83bcb4ba8
KARAM <krm0219@gmail.com>
2021-04-12 11:40:49 +0900

    adapter 추가

--- app/src/main/java/com/kurly/task/searchgithub/ui/main/RepositoryAdapter.kt
+++ app/src/main/java/com/kurly/task/searchgithub/ui/main/RepositoryAdapter.kt
@@ -1,22 +1,62 @@
 package com.kurly.task.searchgithub.ui.main
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.kurly.task.searchgithub.R
+import com.kurly.task.searchgithub.databinding.ItemGithubBinding
 import com.kurly.task.searchgithub.model.RepositoryModel
 
 class RepositoryAdapter(private var repositories: ArrayList<RepositoryModel>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+   ListAdapter<RepositoryModel, RepositoryAdapter.ViewHolder>(RepoDiffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ItemGithubBinding>(layoutInflater, viewType, parent, false)
+        return ViewHolder(binding);
     }
 
+    override fun getItemViewType(position: Int): Int {
+
+        return R.layout.item_github
+    }
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+
+        return super.getItemCount()
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val repository = getItem(position)
+        holder.bind(repository)
     }
+
+    inner class ViewHolder(private val binding: ItemGithubBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(repository: RepositoryModel) {
+
+            // binding.repository = repository
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object RepoDiffUtil : DiffUtil.ItemCallback<RepositoryModel>() {
+        override fun areItemsTheSame(oldItem: RepositoryModel, newItem: RepositoryModel): Boolean {
+
+            // 각 아이템들의 고유한 값을 비
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: RepositoryModel, newItem: RepositoryModel): Boolean {
+
+            return oldItem == newItem
+        }
+    }
+
 }
